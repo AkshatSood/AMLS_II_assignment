1c065c417768441ac0ad44f1430c933cca97ec05
Akshat Sood <soodakshat30@gmail.com>
2023-03-25 22:12:05 +0000

    Filter processed files before performing bicubic interpolation

--- track1/track1.py
+++ track1/track1.py
@@ -24,6 +24,13 @@ class Track1:
             start_time = time.time()
             idx = 1
             target_imgs = self.utility.get_files_in_dir(target['test_dir'])
+
+            # Filter the images that have already been processed
+            result_imgs = self.utility.get_files_in_dir(target['results_dir'])
+            target_imgs = [img for img in target_imgs if img not in result_imgs]
+
+            print(f'\tProcessing {len(target_imgs)} images...')
+
             for img_name in target_imgs:
 
                 self.bicubic_interpolation.run(
