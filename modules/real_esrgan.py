884d444cfd0afc078bd433686c4a385e64976c42
Akshat Sood <soodakshat30@gmail.com>
2023-03-25 23:03:41 +0000

    Add runner for real-esrgan for track1

---
+++ modules/real_esrgan.py
@@ -0,0 +1,32 @@
+import glob
+import os.path as osp
+
+import cv2
+import numpy as np
+import torch
+
+from constants import MODEL_RRDB_ESRGAN_X4, TRACK1_ESRGANX4_TARGETS
+from modules.RRDBNet_arch import RRDBNet
+
+class RealESRGAN(): 
+
+    def __init__(self, device='cuda'):
+        self.device = torch.device(device)
+        self.model = RRDBNet(3, 3, 64, 23, gc=32)
+        self.model.load_state_dict(torch.load(MODEL_RRDB_ESRGAN_X4), strict=True)
+        self.model.eval()
+        self.model = self.model.to(self.device)
+
+    def run(self, input, output):
+        img = cv2.imread(input, cv2.IMREAD_COLOR)
+        img = img * 1.0 / 255
+        img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
+        img_LR = img.unsqueeze(0)
+        img_LR = img_LR.to(self.device)
+
+        with torch.no_grad():
+            upscaled_img = self.model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
+        upscaled_img = np.transpose(upscaled_img[[2, 1, 0], :, :], (1, 2, 0))
+        upscaled_img = (upscaled_img * 255.0).round()
+        cv2.imwrite(output, upscaled_img)
+
--- track1/track1.py
+++ track1/track1.py
@@ -1,22 +1,26 @@
 """Track1 Module"""
 import time
 import cv2
+import torch
 
 from modules.bicubic import BicubicInterpolation
+from modules.real_esrgan import RealESRGAN
 from helpers.utility import Utility
-from constants import TRACK1_BICUBIC_TARGETS
+from constants import TRACK1_BICUBIC_TARGETS, TRACK1_ESRGANX4_TARGETS
 
 class Track1:
 
     def __init__(self):
         self.utility = Utility()
-        self.bicubic_interpolation = BicubicInterpolation()
-
         for target in TRACK1_BICUBIC_TARGETS:
+            self.utility.check_and_create_dir(target['results_dir'])
+        for target in TRACK1_ESRGANX4_TARGETS:
             self.utility.check_and_create_dir(target['results_dir'])
 
     def __run_bicubic_interpolation(self):
         print("Track 1 Bicubic Interpolation")
+
+        bicubic_interpolation = BicubicInterpolation()
 
         for target in TRACK1_BICUBIC_TARGETS:
             print(f'\t{target["name"]}')
@@ -33,7 +37,7 @@ class Track1:
 
             for img_name in target_imgs:
 
-                self.bicubic_interpolation.run(
+                bicubic_interpolation.run(
                     input = f'{target["test_dir"]}/{img_name}',
                     output = f'{target["results_dir"]}/{img_name}',
                     scale = target['scale']
@@ -43,6 +47,37 @@ class Track1:
                     self.utility.progress_print(len(target_imgs), idx, start_time)
                 idx+=1
 
+    def __run_real_esrgan(self): 
+        print("Track 1 ESRGAN")
+
+        for target in TRACK1_ESRGANX4_TARGETS:
+            print(f'\t{target["name"]}')
+
+            
+            start_time = time.time()
+            idx = 1
+            target_imgs = self.utility.get_files_in_dir(target['test_dir'])[:10]
+
+            # Filter the images that have already been processed
+            result_imgs = self.utility.get_files_in_dir(target['results_dir'])
+            target_imgs = [img for img in target_imgs if img not in result_imgs]
+
+            print(f'\tProcessing {len(target_imgs)} images...')
+
+            for img_name in target_imgs:
+                real_esrgan = RealESRGAN(device='cuda')
+                real_esrgan.run(
+                    input = f'{target["test_dir"]}/{img_name}',
+                    output = f'{target["results_dir"]}/{img_name}'
+                )
+                torch.cuda.empty_cache()
+
+                if (idx) % 10 == 0:
+                    self.utility.progress_print(len(target_imgs), idx, start_time)
+                idx+=1
+
+
     def run(self):
-        self.__run_bicubic_interpolation()
+        # self.__run_bicubic_interpolation()
+        self.__run_real_esrgan()
 
